aa2236a132fca6e8e28d8f0b478757a28d76274f
Adamachmad <adamachmad8@gmail.com>
2024-09-24 13:31:29 +0800

    baru ygy

---
+++ sidebar.php
@@ -0,0 +1,30 @@
+<aside id="sidebar" class="sidebar bg-dark text-white p-3">
+    <div class="user mb-3 text-center">
+        <img src="user-image.png" class="user-img rounded-circle mb-2" style="width: 80px; height: 80px;">
+        <div class="deskripsi-profile">
+            <span class="fw-bold">Admin</span><br>
+            <span>Kost Azzahra</span>
+        </div>
+    </div>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a href="client.php" class="nav-link text-white">
+                <i class="fas fa-home me-2"></i> Client
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="iuran.php" class="nav-link text-white">
+                <i class="fas fa-user-tie me-2"></i> Iuran
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="listrik.php" class="nav-link text-white">
+                <i class="fas fa-user-tie me-2"></i> Listrik & Air
+            </a>
+        </li>
+        <li class="nav-item"> <a href="wifi.php" class="nav-link text-white">
+                <i class="fas fa-user-tie me-2"></i> Wifi
+            </a>
+        </li>
+    </ul>
+</aside>
