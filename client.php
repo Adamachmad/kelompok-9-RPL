c0ce9b3ff8f9f02c004f193ca7fc42f2b641868f
dina <zakkiyafitrar@gmail.com>
2024-10-28 14:22:14 +0700

    test

--- client.php
+++ client.php
@@ -34,7 +34,7 @@ $result = $link->query($sql);
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
         <a class="navbar-brand ps-3" href="index.php">Dashboard Kost Azzahra</a>
     </nav>
-    
+    <!-- test -->
     <!-- Sidebar -->
     <?php include 'sidebar.php'; ?>
     
